99b1eb04b53a2034f5831de0985124640d6bdae2
sumanthmk505 <mksumanth12@gmail.com>
2025-01-01 20:52:28 -0800

    first

---
+++ file.py
@@ -0,0 +1,10 @@
+# Program to check if a number is even or odd
+
+# Get input from the user
+num = int(input("Enter a number: "))
+
+# Check condition
+if num % 2 == 0:
+    print(num, "is even.")
+else:
+    print(num, "is odd.")
